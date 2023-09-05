a7e9ad987da2db99ff7b29af08443a6eb30fa475
Vinícius Monteiro de Freitas <viniciusmonteiro30@gmail.com>
2023-09-05 14:58:56 -0300

    Tipo str (string) - Introdução aos tipos de dados

---
+++ aula3.py
@@ -0,0 +1,21 @@
+'''
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+'''
+
+# Aspas simples
+print('Vinícius Monteiro')
+print('Vinícius "Monteiro"') # Aspas dentro do print
+
+# Aspas duplas
+print("Vinícius Monteiro")
+print("Vinícius 'Monteiro'") # Aspas dentro do print
+
+# Escape
+print("Vinícius \"Monteiro\"")
+
+# r
+print(r"Vinícius \"Monteiro\"")
