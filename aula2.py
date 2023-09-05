aac086d0bb0e709a4108fdde04b4a791d18a4f1d
Vinícius Monteiro de Freitas <viniciusmonteiro30@gmail.com>
2023-09-05 14:51:58 -0300

    A função print

---
+++ aula2.py
@@ -0,0 +1,5 @@
+# o print por padrão separa os argumentos com espaço e quebra linha no final
+print(12, 34, 1001, sep='-') # o parametro "sep" permite alterar o separador
+
+print(56, 78, sep='+', end='\n') # o parametro "end" muda o comportamento do final do print (quebra de linha por padrao)
+print(9, 10, sep='-', end='##')
