c532a68989877716d39cf718adb03769491fd3d9
Vinícius Monteiro de Freitas <viniciusmonteiro30@gmail.com>
2023-09-06 19:36:45 -0300

    Introdução às variáveis em Python

---
+++ aula7.py
@@ -0,0 +1,16 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas
+# Pode usar números e underline
+# O sinal de = é o operador de atribuição. Ele é utilizado para atribuir um valor a uma variável.
+# nome_variavel = expressão
+nome_completo = 'Vinícius Monteiro de Freitas'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Vinícius'
+idade = 22
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior de idade?', maior_de_idade)
